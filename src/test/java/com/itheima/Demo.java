34a0ae449d926af71bf21bc2c6e6025b4e0f8c9c
Yan <you@example.com>
2024-02-13 22:38:39 +0800

    第二次提交-阿里云集成

---
+++ src/test/java/com/itheima/Demo.java
@@ -0,0 +1,53 @@
+package com.itheima;
+
+import com.aliyun.oss.ClientException;
+import com.aliyun.oss.OSS;
+import com.aliyun.oss.OSSClientBuilder;
+import com.aliyun.oss.OSSException;
+import com.aliyun.oss.common.auth.CredentialsProviderFactory;
+import com.aliyun.oss.common.auth.EnvironmentVariableCredentialsProvider;
+
+import java.io.FileInputStream;
+import java.io.InputStream;
+
+public class Demo {
+
+    public static void main(String[] args) throws Exception {
+         // Endpoint以华东1（杭州）为例，其它Region请按实际情况填写。
+        String endpoint = "https://oss-cn-hangzhou.aliyuncs.com";
+
+        String accessKeyId = "LTAI5tA3h1rHeBrKBjdvZx4s";
+        String accessKeySecret = "pnV23bC6c9htoJ3zLGPSVXnpx5nVd3";
+
+        // 填写Bucket名称，例如examplebucket。
+        String bucketName = "web-tlias-yanfanyu";
+
+        String objectName = "1.jpg";
+        String filePath = "E:\\images\\1.jpg";
+
+        // 创建OSSClient实例。
+        OSS ossClient = new OSSClientBuilder().build(endpoint, accessKeyId, accessKeySecret);
+
+        try {
+            InputStream inputStream = new FileInputStream(filePath);
+            ossClient.putObject(bucketName, objectName, inputStream);
+
+        } catch (OSSException oe) {
+            System.out.println("Caught an OSSException, which means your request made it to OSS, "
+                    + "but was rejected with an error response for some reason.");
+            System.out.println("Error Message:" + oe.getErrorMessage());
+            System.out.println("Error Code:" + oe.getErrorCode());
+            System.out.println("Request ID:" + oe.getRequestId());
+            System.out.println("Host ID:" + oe.getHostId());
+        } catch (ClientException ce) {
+            System.out.println("Caught an ClientException, which means the client encountered "
+                    + "a serious internal problem while trying to communicate with OSS, "
+                    + "such as not being able to access the network.");
+            System.out.println("Error Message:" + ce.getMessage());
+        } finally {
+            if (ossClient != null) {
+                ossClient.shutdown();
+            }
+        }
+    }
+}
