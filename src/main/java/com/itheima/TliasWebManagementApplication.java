a48f417798283e1ddb288efb3fd6c3264966fbf5
Yan <you@example.com>
2024-02-17 22:31:58 +0800

    第二次提交-阿里云集成

--- src/main/java/com/itheima/TliasWebManagementApplication.java
+++ src/main/java/com/itheima/TliasWebManagementApplication.java
@@ -2,7 +2,9 @@ package com.itheima;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 
+@ServletComponentScan   // 开启了对Servlet组件的支持
 @SpringBootApplication
 public class TliasWebManagementApplication {
 
--- src/main/java/com/itheima/filter/DemoFilter.java
+++ src/main/java/com/itheima/filter/DemoFilter.java
@@ -1,22 +1,30 @@
-package com.itheima.filter;
-
-import javax.servlet.*;
-import java.io.IOException;
-
-public class DemoFilter implements Filter {
-
-    @Override   // 只执行一次
-    public void init(FilterConfig filterConfig) throws ServletException {
-        Filter.super.init(filterConfig);
-    }
-
-    @Override   // 拦截到请求之后调用，执行多次
-    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
-
-    }
-
-    @Override   // 销毁操作，只执行一次
-    public void destroy() {
-        Filter.super.destroy();
-    }
-}
+//package com.itheima.filter;
+//
+//import javax.servlet.*;
+//import javax.servlet.annotation.WebFilter;
+//import java.io.IOException;
+//
+//
+////@WebFilter(urlPatterns = "/*")
+//public class DemoFilter implements Filter {
+//
+//    @Override   // 只执行一次
+//    public void init(FilterConfig filterConfig) throws ServletException {
+//        Filter.super.init(filterConfig);
+//        System.out.println("初始化");
+//    }
+//
+//    @Override   // 拦截到请求之后调用，执行多次
+//    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+//        System.out.println("拦截");
+//
+//        // 放行
+//        filterChain.doFilter(servletRequest, servletResponse);
+//    }
+//
+//    @Override   // 销毁操作，只执行一次
+//    public void destroy() {
+//        Filter.super.destroy();
+//        System.out.println("销毁了");
+//    }
+//}
